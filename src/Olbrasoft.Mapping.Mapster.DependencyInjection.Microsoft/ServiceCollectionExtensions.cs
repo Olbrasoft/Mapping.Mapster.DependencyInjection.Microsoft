154f83fdd93fc961d9f7636a2aa24a72b78dcc7c
MatrixStore <matrixstore@email.cz>
2020-03-22 00:24:09 +0100

    s

---
+++ src/Olbrasoft.Mapping.Mapster.DependencyInjection.Microsoft/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Olbrasoft.Mapping.Mapster.DependencyInjection.Microsoft
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddProjection(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddMapster(assemblies);
+
+            services.AddScoped<IMapper, MapsterMapper>();
+        }
+    }
+}
